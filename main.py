d61c175aad20de59aa461b8d73254d86322c1730
Ram81 <ramramrakhya81@gmail.com>
2023-02-01 14:46:43 -0500

    [Revert] Use default dataset loader

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import gzip
 
 from tqdm import tqdm
 
-from prior import NoCacheLazyJsonDataset
+from prior import LazyJsonDataset
 
 
 def load_dataset() -> prior.DatasetDict:
@@ -12,7 +12,7 @@ def load_dataset() -> prior.DatasetDict:
     for split, size in [("train", 100_000)]:
         with gzip.open(f"{split}.jsonl.gz", "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
-        data[split] = NoCacheLazyJsonDataset(
+        data[split] = LazyJsonDataset(
             data=houses, dataset="procthor-100k", split=split
         )
     return prior.DatasetDict(**data)

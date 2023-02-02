191f16eea993ddcddf5ed7377a0bc9f9eeff1cb6
Kunal Pratap Singh <krkunalking@gmail.com>
2023-02-01 16:13:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import prior
 import gzip
-
+import wget
 from tqdm import tqdm
 
 from prior import LazyJsonDataset
@@ -10,6 +10,9 @@ def load_dataset() -> prior.DatasetDict:
     """Load the houses dataset."""
     data = {}
     for split, size in [("train", 100_000)]:
+        if not f"{split}.jsonl.gz" in os.listdir('./'):
+                print (os.listdir('./'))
+                wget.download("https://anonymous-neurips22.s3.us-west-2.amazonaws.com/a4h/train.jsonl.gz")
         with gzip.open(f"{split}.jsonl.gz", "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
         data[split] = LazyJsonDataset(
